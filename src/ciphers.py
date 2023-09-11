820ef42035d10343d4360624d59219ad121253a6
Jan Provaznik <janpro@janpro.dev>
2023-09-11 17:27:27 +0200

    (refactor) ciphers: add types, remove useless code (remove) utils testing code (add) start wrtiting reproducible copy random text script

--- src/ciphers.py
+++ src/ciphers.py
@@ -1,70 +1,73 @@
 import random
 import numpy as np
 from enigma.machine import EnigmaMachine
+
 rand = random.Random(42)
 
-def substitute(text, mapping):
+
+def substitute(text: str, mapping: dict[str, str]):
     """
     mapping is a dict that for each letter in the alphabet
     maps to another letter in the alphabet
-    
+
     enusre the map is a bijection!
     """
-    return ''.join(mapping.get(c, c) for c in text)
+    return "".join(mapping.get(c, c) for c in text)
 
-def create_substitution_dict(permutation):
+
+def create_substitution_dict(permutation: list[str]) -> dict[str, str]:
     """
     permutation is a list of letters in the alphabet
     """
-    d = {chr(ord('A') + i): permutation[i] for i in range(len(permutation))}
-    d[' '] = ' '
+    d = {chr(ord("A") + i): permutation[i] for i in range(len(permutation))}
+    d[" "] = " "
     return d
 
-# this should be impossible to crack without frequence analysis
-def random_substitution(text):
+
+# this should be impossible to crack without frequence analysis on large dataset
+def random_substitution(text: str) -> tuple[str, dict[str, str]]:
     """
     substitution cipher should be invertible
     """
     perm = np.random.permutation(26)
-    perm_str = [chr(ord('A') + i) for i in perm]
+    perm_str = [chr(ord("A") + i) for i in perm]
     mapping = create_substitution_dict(perm_str)
     return (substitute(text, mapping), mapping)
 
-def nothing(text):
+
+def nothing(text: str) -> str:
     return text
 
-# Caesar
-def caesar(text, shift=3):
-    return ''.join(chr((ord(c) - ord('A') + shift) % 26 + ord('A')) for c in text)
 
-def caesar_random_hint(text):
+# Caesar
+def caesar(text: str, shift: int = 3) -> str:
+    return "".join(chr((ord(c) - ord("A") + shift) % 26 + ord("A")) for c in text)
+
+
+def caesar_random_hint(text: str) -> str:
     shift = rand.randint(0, 25)
     # convert shift to char
-    shift_char = chr(ord('A') + shift)
+    shift_char = chr(ord("A") + shift)
     return f"{shift_char}{caesar(text, shift)}"
 
-def caesar_random(text):
+
+def caesar_random(text: str) -> str:
     shift = rand.randint(0, 25)
     return caesar(text, shift)
 
-# Enigma
-machine = None
-def enigma_encrypt_all_the_same(text, start_display='ABC', ring_settings=[0,0,0]):
-    if machine == None:
-       machine = EnigmaMachine.from_key_sheet(
-       rotors='I II III',
-       reflector='B',
-       ring_settings=ring_settings,
-       plugboard_settings=None)
-    machine.set_display(start_display)
-    return f"{start_display}{machine.process_text(text)}"
 
-def enigma_encrypt_random(text, seed=42):
-    machine = EnigmaMachine.from_key_sheet(
-        rotors='I II III',
-        reflector='B',
-        ring_settings=[0,0,0],
-        plugboard_settings=None)
-    start_display = ''.join(rand.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(3))
+machine = None
+
+
+def enigma_encrypt_all_the_same(
+    text: str, start_display: str = "ABC", ring_settings: list[int] = [0, 0, 0]
+) -> str:
+    if machine == None:
+        machine = EnigmaMachine.from_key_sheet(
+            rotors="I II III",
+            reflector="B",
+            ring_settings=ring_settings,
+            plugboard_settings=None,
+        )
     machine.set_display(start_display)
     return f"{start_display}{machine.process_text(text)}"
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,14 @@
+import os
+import logging
 
-import requests
-import zipfile
-def download_dataset(args):
-    # download from link and unzip to dataset_path
-
-    url = args.download_dataset
-    r = requests.get(url, allow_redirects=True)
-    open('data.zip', 'wb').write(r.content)
+def download_newscrawl(year=2012, language="en"):
+    newscrawl_url = f"https://data.statmt.org/news-crawl/{language}/"
+    filename = f"news.{year}.{language}.shuffled.deduped.gz"
+    url = newscrawl_url + filename
+    logging.info(f"Downloading {filename} from {url}")
+    os.system(f"wget {url}")
+    os.system(f"gunzip {filename}")
+    logging.info(
+        f"Downloaded and extracted {filename}",
+        f"full path: {os.path.abspath(filename)}",
+    )
