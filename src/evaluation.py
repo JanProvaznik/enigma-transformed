95aec7780ae1d00f920a921f05bc1b0e12a89cf6
Jan Provaznik <janpro@janpro.dev>
2024-01-04 01:42:26 +0100

    (add) evaluating models

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Type, Optional
+from src.ByT5Dataset import ByT5Dataset
 from scipy.spatial.distance import jensenshannon
 
 def js_divergence(dict1, dict2):
@@ -16,3 +19,19 @@ def js_divergence(dict1, dict2):
     divergence = jensenshannon(prob_dist1, prob_dist2)
     
     return divergence
+
+@dataclass
+class Model:
+    dataset_class: Type[ByT5Dataset]
+    name: str
+    language: str
+    slurm_id: int
+    is_checkpoint: bool = False
+    checkpoint_step: int = 0
+    noise_proportion: Optional[float] = None
+
+    def path(self):
+        if self.is_checkpoint:
+            return f"./logs/{self.slurm_id}/output/checkpoint-{self.checkpoint_step}"
+        else:
+            return f"./logs/{self.slurm_id}/model"
